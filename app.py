8ca3e101663d10fb3f9a789a203b32960f663ea8
twelcone <vuquocminhdang@gmail.com>
2024-05-31 00:02:32 +0700

    Initial upload source code

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+from PIL import Image
+import pandas as pd
+from ultralytics import YOLO
+import cv2
+
+@st.cache_resource
+def load_model():
+    model = YOLO("weight/yolov8l-best.pt")
+    return model
+
+model = load_model()
+
+st.title("Logo Detection Demo")
+
+st.subheader("Upload an Image")
+upload = st.file_uploader("Upload an image (PNG, JPG, or JPEG)", type=["png", "jpg", "jpeg"])
+
+st.subheader("Set Confidence Threshold (Optional)")
+confidence_threshold = st.slider("Confidence Threshold", min_value=0.0, max_value=1.0, value=0.8, step=0.05)
+
+if upload:
+    try:
+        img = Image.open(upload)
+        with st.spinner("Predicting..."):
+            results = model.predict(img, conf=confidence_threshold)
+        
+        result = results[0] 
+        
+        annotated_img = cv2.cvtColor(result.plot() , cv2.COLOR_BGR2RGB)
+        st.image(annotated_img, caption="Detected Logos")
+
+        st.subheader("Detailed Results")
+        if result.boxes:
+            data = []
+            for box in result.boxes:
+                label = result.names[box.cls[0].item()]
+                confidence = box.conf[0].item()
+                coordinates = box.xywh[0].tolist()
+                data.append([label, confidence, coordinates])
+
+            df = pd.DataFrame(data, columns=["Label", "Confidence", "Coordinates"])
+
+            # Split Coordinates and round values
+            df[['Center X', 'Center Y', 'Width', 'Height']] = pd.DataFrame(df['Coordinates'].tolist(), index=df.index).round(0).astype("int")
+            df = df.drop(columns=['Coordinates'])
+
+            st.table(df)  # Display the modified DataFrame
+
+        else:
+            st.write("No logos detected.")
+
+    except Exception as e:
+        st.error(f"Error processing the image: {e}")
